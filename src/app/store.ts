dff5991575c273737f11f040142e886be0751b1f
Richard Leshik <richardleshik@gmail.com>
2023-08-11 20:08:48 +0300

    add redux toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,11 @@
 import { tasksReducer } from 'features/TodolistsList/tasks-reducer'
 import { todolistsReducer } from 'features/TodolistsList/todolists-reducer'
-import { AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
+import { AnyAction, combineReducers } from 'redux'
 import thunkMiddleware, { ThunkDispatch } from 'redux-thunk'
 import { useDispatch } from 'react-redux'
 import { appReducer } from './app-reducer'
 import { authReducer } from 'features/Login/auth-reducer'
+import { configureStore } from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
   app: appReducer,
@@ -13,9 +14,12 @@ const rootReducer = combineReducers({
   todolists: todolistsReducer
 })
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
+})
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
+export type AppRootStateType = ReturnType<typeof store.getState>
 export type AppThunkDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatchType>()
