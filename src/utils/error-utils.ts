11b174613ac0179a391744670d5a3a7ca69b95f7
Richard Leshik <richardleshik@gmail.com>
2023-10-01 22:38:01 +0300

    change handleServerNetworkError util

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,7 +1,8 @@
 import { appActions } from 'app/app-reducer'
 import { Dispatch } from 'redux'
-import { ResponseType } from 'api/todolist-api'
-import { tasksActions } from 'features/TodolistsList/tasks-reducer'
+import { ResponseType } from 'api/todolists-api'
+import axios from 'axios'
+import { AppThunkDispatchType } from 'app/store'
 
 // generic function
 export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: Dispatch) => {
@@ -13,7 +14,23 @@ export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: Dispatc
   dispatch(appActions.setAppStatus({ status: 'failed' }))
 }
 
-export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch) => {
+export const handleServerNetworkErrorOldVersion = (error: { message: string }, dispatch: Dispatch): void => {
   dispatch(appActions.setAppError({ error: error.message }))
+  dispatch(appActions.setAppStatus({ status: 'failed' }))
+}
+
+export const handleServerNetworkError = (err: unknown, dispatch: AppThunkDispatchType): void => {
+  let errorMessage = 'Some error occurred'
+
+  if (axios.isAxiosError(err)) {
+    // @ts-ignore
+    errorMessage = err.response?.data?.message || err?.message || errorMessage
+  } else if (err instanceof Error) {
+    errorMessage = `Native error: ${err.message}`
+  } else {
+    errorMessage = JSON.stringify(err)
+  }
+
+  dispatch(appActions.setAppError({ error: errorMessage }))
   dispatch(appActions.setAppStatus({ status: 'failed' }))
 }
