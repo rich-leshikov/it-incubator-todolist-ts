355e68b150bb067d809e5237d9c643c5c8a54036
Richard Leshik <richardleshik@gmail.com>
2023-10-29 23:59:20 +0300

    add thunkTryCatch util function

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,4 @@
 export {handleServerNetworkError} from './handleServerNetworkError'
 export {handleServerAppError} from './handleServerAppError'
 export {createAppAsyncThunk} from './createAppAsyncThunk'
+export {thunkTryCatch} from './thunkTryCatch'
---
+++ src/common/utils/thunkTryCatch.ts
@@ -0,0 +1,23 @@
+import { AppThunkDispatchType, AppRootStateType } from 'app/store'
+import { handleServerNetworkError } from 'common/utils'
+import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk'
+import { appActions } from 'app/app.reducer'
+import { ResponseType } from 'common/api/common.api'
+
+export const thunkTryCatch = async <T>(
+  thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppThunkDispatchType, null | ResponseType>,
+  logic: () => Promise<T>
+): Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>> => {
+  const { dispatch, rejectWithValue } = thunkAPI
+
+  dispatch(appActions.setAppStatus({ status: 'loading' }))
+
+  try {
+    return await logic()
+  } catch (err) {
+    handleServerNetworkError(err, dispatch)
+    return rejectWithValue(null)
+  } finally {
+    dispatch(appActions.setAppStatus({ status: 'idle' }))
+  }
+}
